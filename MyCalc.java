d309ded4a16c313b93b428e567dbe16fd5c65208
Ghostboy007 <96197284+Ghostboy007@users.noreply.github.com>
2024-02-28 22:08:07 +0530

    Create MyCalc.java

---
+++ MyCalc.java
@@ -0,0 +1,37 @@
+package cu.devops;
+public class MyCalc {
+	
+	public int sum(int a, int b)
+	{
+		return a+b;
+	}
+	public int diff(int a, int b)
+	{
+		return a-b;
+	}
+	public int div(int a, int b)
+	{
+		return a/b;
+	}
+	public int mul(int a, int b)
+	{
+		return a*b;
+	}
+	public void show()
+	{
+		System.out.println("Show function called");
+	}
+	public static void main(String[] args) {
+
+
+		MyCalc calc = new MyCalc();
+		System.out.println("Sum is "+calc.sum(10, 5));
+		System.out.println("Diff is "+calc.diff(10, 5));
+		System.out.println("Div is "+calc.div(10, 5));
+		System.out.println("Mul is "+calc.mul(10, 5));
+
+
+		calc.show();
+	}
+
+}
