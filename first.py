4024d9a0f7c6e14a2edba721bc738a3ca6d6e8d5
Ghostboy007 <96197284+Ghostboy007@users.noreply.github.com>
2024-02-29 22:05:50 +0530

    Update first.py

--- first.py
+++ first.py
@@ -5,4 +5,4 @@ num1 = 5
 num2 = 7
 result = add_two_numbers(num1, num2)
 print("The sum of", num1, "and", num2, "is:", result)
-print("Hello Sanskriti 21BCS5974")
+print("Hello Sanskriti 21BCS5924")
